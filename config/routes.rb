b3cba8ca4cc057af6597f771172232e3e9f3fc9b
trustpizza <maxelolsson@gmail.com>
2022-12-18 16:32:52 -0600

    Add navbar to top of every page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "posts#index"
+
+  resources :users do
+    resources :posts, only: [:index, :new, :create]
+    resources :friend_requests
+    resources :friendships
+  end
+
+  resources :posts, only: [:show, :edit, :update, :destroy] do
+    resources :comments
+    resources :likes
+  end
 end
