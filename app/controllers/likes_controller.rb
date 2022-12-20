77ff0793ad2fc21f322650195fa03a5dbdc69f76
trustpizza <maxelolsson@gmail.com>
2022-12-20 17:02:04 -0600

    Turbo stream unknown format

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,13 +6,23 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(like_params[:post_id])
     @like = @post.likes.create(like_params)
+
+    respond_to  do |format|
+    end
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     @like = @post.likes.find_by(like_params)
-    @like.destroy
-
+    #debugger
+    
+    respond_to do |format|
+      if @like.destroy
+        format.turbo_stream
+      else
+        redirect_to root_url  
+      end
+    end
   end
 
   private
