5da2d98b97d3317838ceb6432ca04496d981299f
trustpizza <maxelolsson@gmail.com>
2022-12-19 22:35:27 -0600

    Begin to work on turboframe for new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,7 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.build(post_params)
 
+  
     if @post.save
       redirect_to root_path
       flash[:notice]='Post was created'
